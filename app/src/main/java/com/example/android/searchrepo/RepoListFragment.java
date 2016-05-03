d954b58d75b2c964bb639f90e3b820a598436d3f
mndiv <nagadivya.m@gmail.com>
2016-05-03 12:47:39 -0400

    Parse Json String

--- app/src/main/java/com/example/android/searchrepo/RepoListFragment.java
+++ app/src/main/java/com/example/android/searchrepo/RepoListFragment.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,6 +15,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -31,6 +36,7 @@ import java.util.List;
 public class RepoListFragment extends Fragment {
 
     ArrayAdapter<String> mRepoAdapter;
+
     public RepoListFragment() {
     }
 
@@ -48,13 +54,13 @@ public class RepoListFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //Handle item selection
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_refresh:
                 FetchRepoTask repoTask = new FetchRepoTask();
                 repoTask.execute("stars:>1");
                 return true;
             default:
-                    return super.onOptionsItemSelected(item);
+                return super.onOptionsItemSelected(item);
         }
 
     }
@@ -62,7 +68,7 @@ public class RepoListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
 
         String[] data = {
@@ -80,29 +86,75 @@ public class RepoListFragment extends Fragment {
                 "twbs/bootstrap"
         };
 
-        List<String> repoList  = new ArrayList<String>(Arrays.asList(data));
+        List<String> repoList = new ArrayList<String>(Arrays.asList(data));
 
         mRepoAdapter = new ArrayAdapter<String>(getActivity(),
                 R.layout.list_item_repo,
                 R.id.list_item_repo_textView,
                 repoList);
 
-        ListView listView = (ListView)rootView.findViewById(R.id.listitem_repo);
+        ListView listView = (ListView) rootView.findViewById(R.id.listitem_repo);
         listView.setAdapter(mRepoAdapter);
-
-
 
 
         return rootView;
     }
 
-    public class FetchRepoTask extends AsyncTask<String, Void, Void>{
+    public class FetchRepoTask extends AsyncTask<String, Void, String[]> {
 
         private final String LOG_TAG = FetchRepoTask.class.getSimpleName();
-        @Override
-        protected Void doInBackground(String... params) {
 
-            if(params.length == 0){
+        private String[] getRepoDataFromJson(String repoJsonStr)
+                throws JSONException {
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_ITEMS = "items";
+            final String OWM_NAME = "full_name";
+            final String OWM_DESCRIPTION = "description";
+            final String OWM_LANGUAGE = "language";
+            final String OWM_UPDATED = "updated_at";
+
+
+            JSONObject repoJson = new JSONObject(repoJsonStr);
+            JSONArray repoArray = repoJson.getJSONArray(OWM_ITEMS);
+
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            // we start at the day returned by local time. Otherwise this is a mess.
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            dayTime = new Time();
+
+            int numRepos = repoArray.length();
+            String[] resultStrs = new String[numRepos];
+
+
+            for(int i=0;i < numRepos;i++ ){
+                String fullName, description, language, updated;
+
+                JSONObject eachRepo = repoArray.getJSONObject(i);
+                fullName = eachRepo.getString(OWM_NAME);
+                description = eachRepo.getString(OWM_DESCRIPTION);
+                language = eachRepo.getString(OWM_LANGUAGE);
+                updated = eachRepo.getString(OWM_UPDATED);
+
+                resultStrs[i] = fullName + " - " + description + " - " +language + " - " + updated;
+            }
+
+            for(String s:resultStrs)
+                Log.v(LOG_TAG, "Repositories: " + s);
+
+            return resultStrs;
+
+
+
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            if (params.length == 0) {
                 return null;
             }
             // These two need to be declared outside the try/catch
@@ -115,7 +167,6 @@ public class RepoListFragment extends Fragment {
             String repoJsonStr = null;
 
 
-
 //        GithubReposClient client = new RepoSearchClient(getActivity(), username);
 //        client.observable()
 //                .observeOn(AndroidSchedulers.mainThread()
@@ -123,13 +174,13 @@ public class RepoListFragment extends Fragment {
 //                        .subscribe( ... );
 
 
-            try{
+            try {
                 // Construct the URL for the github query
                 // Possible parameters are avaiable at Github API page, at
                 // hhttps://developer.github.com/v3/search/#search-repositories
 
 
-               // URL url = new URL("https://api.github.com/search/repositories?q=stars:>1");
+                // URL url = new URL("https://api.github.com/search/repositories?q=stars:>1");
 
                 final String REPO_BASE_URL = "https://api.github.com/search/repositories?";
                 final String QUERY_PARAM = "q";
@@ -150,7 +201,7 @@ public class RepoListFragment extends Fragment {
                 Log.v(LOG_TAG, "Built URI " + builtUri.toString());
 
                 // Create the request to Github, and open the connection
-                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
 
@@ -176,24 +227,32 @@ public class RepoListFragment extends Fragment {
                     return null;
                 }
                 repoJsonStr = buffer.toString();
-                Log.v("RepoListFragment","repoJsonString : \n" + repoJsonStr);
-            }catch (IOException e){
+                Log.v("RepoListFragment", "repoJsonString : \n" + repoJsonStr);
+            } catch (IOException e) {
                 Log.e("RepoListFragment", "Error ", e);
                 // If the code didn't successfully get the repositories data, there's no point in attemping
                 // to parse it.
                 return null;
-            }finally {
-                if(urlConnection != null){
+            } finally {
+                if (urlConnection != null) {
                     urlConnection.disconnect();
 
                 }
-                if(reader != null){
-                    try{
+                if (reader != null) {
+                    try {
                         reader.close();
-                    }catch (final IOException e){
+                    } catch (final IOException e) {
                         Log.e("RepoListFragment", "Error closing stream", e);
                     }
                 }
+            }
+
+            try {
+                //Log.v(LOG_TAG,"No. of repos in page = " + getRepoDataFromJson(repoJsonStr));
+                return getRepoDataFromJson(repoJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage() + e);
+                e.printStackTrace();
             }
 
             return null;
