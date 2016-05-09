f401f09b54f9840fa30fc6d865123bb1b2914f87
mndiv <nagadivya.m@gmail.com>
2016-05-09 01:00:03 -0400

    Create ShareIntent in detail fragment

--- app/src/main/java/com/example/android/searchrepo/RepoDetailFragment.java
+++ app/src/main/java/com/example/android/searchrepo/RepoDetailFragment.java
@@ -1,9 +1,15 @@
 package com.example.android.searchrepo;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,7 +19,12 @@ import android.widget.TextView;
  */
 public class RepoDetailFragment extends Fragment {
 
+    private static final String LOG_TAG = RepoDetailFragment.class.getSimpleName();
+    private String repoStr;
+    private static final String REPO_SHARE_HASHTAG = "#GitSearchRepo";
+
     public RepoDetailFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -22,9 +33,40 @@ public class RepoDetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         Intent intent = getActivity().getIntent();
-        String repoStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+        repoStr = intent.getStringExtra(Intent.EXTRA_TEXT);
 
         ((TextView) rootView.findViewById(R.id.repo_detail_textView)).setText(repoStr);
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.repodetailfragment, menu);
+
+        // Retrieve the share menu item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+        ShareActionProvider mShareActionProvider =
+        (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        // Attach an intent to this ShareActionProvider.  You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        if (mShareActionProvider != null ) {
+        mShareActionProvider.setShareIntent(createShareRepoIntent());
+        } else {
+        Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+    }
+
+    private Intent createShareRepoIntent() {
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                repoStr+REPO_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
